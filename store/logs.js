b8165a06605c85c99ef16c80a571e0fc23f454c9
Oskar van Velden <oskar@rakso.nl>
2020-04-27 23:03:12 +0200

    Fetch kooklog-store from aws

--- store/logs.js
+++ store/logs.js
@@ -1,63 +1,39 @@
+const ADD_ENTRY = 'ADD_ENTRY';
+const DELETE_ENTRY = 'DELETE_ENTRY';
+const SET_ENTRIES = 'SET_ENTRIES';
+const SET_LAST_MUTATION_DATE = 'SET_LAST_MUTATION_DATE';
+
 export const state = () => ({
   lastMutationDate: '',
-  entries: [
-    {
-      name: 'Rijst met kipkerrie',
-      date: Date.parse('16 April 2020'),
-    },
-    {
-      name: 'Pannenkoeken',
-      date: Date.parse('17 April 2020'),
-    },
-    {
-      name: 'Groentensoep',
-      date: Date.parse('18 April 2020'),
-    },
-    {
-      name: 'Gebakken aardappeltjes + kip (ala oma)',
-      date: Date.parse('19 April 2020'),
-    },
-    {
-      name: 'Pasta ala Tobias',
-      date: Date.parse('15 April 2020'),
-    },
-    {
-      name: 'Patat',
-      date: Date.parse('14 April 2020'),
-    },
-  ],
+  entries: [],
 });
 
 export const mutations = {
-  ADD_ENTRY(state, entry) {
+  [ADD_ENTRY](state, entry) {
     state.entries.push(entry);
   },
-  DELETE_ENTRY(state, { entry }) {
+  [DELETE_ENTRY](state, { entry }) {
     state.list.splice(state.entries.indexOf(entry), 1);
   },
-  SET_ENTRIES(state, entries) {
+  [SET_ENTRIES](state, entries) {
     state.entries = [...entries];
+  },
+  [SET_LAST_MUTATION_DATE](state, lastMutationDate) {
+    state.lastMutationDate = lastMutationDate;
   },
 };
 
 export const actions = {
   async fetchInitialStore({ commit }) {
-    console.log('commit : ', commit);
+    const { data } = await this.$axios.get(
+      'https://rakso-kooklog-store.s3.eu-west-2.amazonaws.com/kooklog-store.json',
+    );
 
-    console.log('nuxtServerInit');
-    //
-    const { data } = await this.$axios.get('http://swapi.dev/api/starships/');
-    const entries = data.results.map(result => ({
-      name: result.name,
-      date: Date.parse(result.created),
-    }));
-
-    console.log('entries : ', entries);
-
-    commit('SET_ENTRIES', entries);
+    commit(SET_ENTRIES, data.entries);
+    commit(SET_LAST_MUTATION_DATE, data.lastMutationDate);
   },
   addEntry({ commit }, { name, date }) {
-    commit('ADD_ENTRY', {
+    commit(ADD_ENTRY, {
       name,
       date: Date.parse(date),
     });
