151e87739fa743de23453e0e4462581d822159cf
Oskar van Velden <oskar@rakso.nl>
2020-04-28 23:31:01 +0200

    Display text when list is empty

--- pages/list.vue
+++ pages/list.vue
@@ -2,30 +2,35 @@
   <v-row>
     <v-col cols="12" sm="12" md="6">
       <v-card max-width="600" class="mx-auto">
-        <v-list two-line subheader>
-          <v-list-item
-            v-for="entry in sortedEntries"
-            :key="entry.title"
-            @click="clickHandler"
-          >
-            <v-list-item-avatar>
-              <v-icon :class="[entry.iconClass]" v-text="entry.icon"></v-icon>
-            </v-list-item-avatar>
+        <template v-if="sortedEntries.length">
+          <v-list two-line subheader>
+            <v-list-item
+              v-for="entry in sortedEntries"
+              :key="entry.title"
+              @click="clickHandler"
+            >
+              <v-list-item-avatar>
+                <v-icon :class="[entry.iconClass]" v-text="entry.icon" />
+              </v-list-item-avatar>
 
-            <v-list-item-content>
-              <v-list-item-title v-text="entry.name"></v-list-item-title>
-              <v-list-item-subtitle
-                v-text="formatDate(entry.date)"
-              ></v-list-item-subtitle>
-            </v-list-item-content>
+              <v-list-item-content>
+                <v-list-item-title v-text="entry.name" />
+                <v-list-item-subtitle v-text="formatDate(entry.date)" />
+              </v-list-item-content>
 
-            <v-list-item-action>
-              <v-btn icon>
-                <v-icon color="grey lighten-1">mdi-information</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </v-list-item>
-        </v-list>
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon color="grey lighten-1">mdi-information</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </template>
+        <v-list-item v-else>
+          <v-list-item-content>
+            <v-list-item-title>Nog niks in de lijst</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-card>
     </v-col>
   </v-row>
