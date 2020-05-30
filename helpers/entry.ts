ce0f1b461d2dfe9dc22d886cd2369e0eaa211362
Oskar van Velden <oskar@rakso.nl>
2020-05-30 22:52:23 +0200

    Add notes field to Entry-type

--- helpers/entry.ts
+++ helpers/entry.ts
@@ -3,9 +3,15 @@ import { DateTime } from 'luxon';
 export interface Entry {
   date: string;
   dishId: number;
+  notes: string;
 }
 
-export const makeEntry = (date: DateTime, dishId: number): Entry => ({
+export const makeEntry = (
+  date: DateTime,
+  dishId: number,
+  notes: string = '',
+): Entry => ({
   date: date.toISODate(),
   dishId,
+  notes,
 });
